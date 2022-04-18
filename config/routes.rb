2f49a36c3ae6a7b6492636972d8c60ce0f167cc6
Breanne <BreMarotta@gmail.com>
2022-04-18 14:50:58 -0400

    set up beginning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  post '/signup', to: 'users#create'
+
+  resources :books
   
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
