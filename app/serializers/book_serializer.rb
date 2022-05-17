7194ce32f9992fc8078eb232833f8132c1d6eb22
Breanne <BreMarotta@gmail.com>
2022-05-17 12:17:53 -0400

    add belongs_to :user to books_controller

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,5 +2,6 @@ class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :favorite_quote, :rating, :lent, :quotes
 
   belongs_to :category
+  belongs_to :user
   has_many :quotes
 end
