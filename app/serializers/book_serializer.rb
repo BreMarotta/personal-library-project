b94ae8d72f3779f38defa3de4a6223ed488767dd
Breanne <BreMarotta@gmail.com>
2022-05-17 12:26:30 -0400

    remove belong_to :user

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,6 +2,5 @@ class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :favorite_quote, :rating, :lent, :quotes
 
   belongs_to :category
-  belongs_to :user
   has_many :quotes
 end
